a2879def632cf0c7e46209e59c9382cab1c33586
Juan Carlos Santamaria <jsantamaria@namutek.com>
2020-11-15 20:44:18 -0600

    Creacion del Prouyecto para Cosmos DB

---
+++ BlobConsoleTest/CosmosTableConsole/AppSettings.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CosmosTableConsole
+{
+    /// <summary>
+    /// Esta clase realiza el mapeo de todas mis
+    /// propiedades defenidas previamente
+    /// para automaticamente mapearlas
+    /// Las cuales debe de defenirse en el igual 
+    /// que mi archivivo de configuraccion
+    /// en formato json. appsettings.json
+    /// </summary>
+    public class AppSettings
+    {
+        public string StorageConnectionString { get; set; }
+        public string DefaulConnString { get; set; }
+
+        /// <summary>
+        /// Defino el metodo para mapear 
+        /// todos mis propiedades 
+        /// con las conexiones que tenga. 
+        /// </summary>
+        /// <returns></returns>
+        public static AppSettings LoadAppSettings()
+        {
+            IConfigurationRoot configRoot = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            AppSettings appSettings = configRoot.Get<AppSettings>();
+            return appSettings;
+        }
+    }
+}
---
+++ BlobConsoleTest/CosmosTableConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace CosmosTableConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = Common.CreateTableAsync("Tabla2");
+            var res2 = Common.CreateTable("Table3");
+
+            Console.WriteLine($"Ejecutado Correctamente {res.Status} or {res2.Name}");
+
+        }
+
+        public static async Task<CloudTable> CreateTableAsyc(string tableName, string conectionStrig)
+        {
+            try
+            {
+                // Nos Conectamos
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(conectionStrig);
+
+                // Este Cliente esteblece la conexion de storage a mi cosmos.
+                CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+
+                //ya Tengo mi tabla que exista segun la configuracion anterior
+                CloudTable table = tableClient.GetTableReference(tableName);
+
+                if (await table.CreateIfNotExistsAsync())
+                    Console.WriteLine($"Tabla Creada {tableName}");
+                else
+                    Console.WriteLine($"La tabla ya existe {tableName}");
+
+                return table;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+         
+        }
+
+    }
+}
